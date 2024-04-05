c72729270ca9c6eff5373505401f79363a943622
maegon <dlagorhs99@naver.com>
2024-04-05 18:48:09 +0900

    콘솔에서 가능한 것들만 제작해보기(반기각 시작)

---
+++ src/main/java/org/example/FirstView/App.java
@@ -0,0 +1,17 @@
+package org.example.FirstView;
+
+
+public class App {
+    public static void start() {
+        // 시작화면은 앱에서 제작해야함.
+        System.out.println("========= Main Menu =========");
+        System.out.println("공지 사항: 공지사항"); //
+        System.out.println("자유 게시판: 자유게시판"); //
+        System.out.println("EDM 인기 차트: 인기차트"); // 함수로 바로 띄울것
+        System.out.println("EDM 장르별 추천: 테크노, 하우스, 덥스텝, 트랜스, 드럼 앤 베이스, 드럼스텝, 트랩, 풋워크, 리믹스"); //
+        System.out.println("검색어 입력: DJ명, 음악 제목"); //
+        System.out.println("로그인하기: 로그인"); //
+        System.out.println("앱 종료: 종료"); //
+
+    }
+}
--- src/main/java/org/example/util/App.java
+++
@@ -1,7 +0,0 @@
-package org.example.util;
-
-public class App {
-    public void start() {
-
-    }
-}
