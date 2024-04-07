f6cfdeb68c85d80690aa513646157faa1d26cb6a
maegon <dlagorhs99@naver.com>
2024-04-07 14:27:13 +0900

    앱종료 구현

--- src/main/java/org/example/FirstView/App.java
+++ src/main/java/org/example/FirstView/App.java
@@ -1,18 +1,24 @@
 package org.example.FirstView;
 
 
+import org.example.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 
 public class App {
     Scanner sc = new Scanner(System.in);
+
     public void start() {
+
         // 시작화면은 앱에서 제작해야함.
 
         
         // 메인 로고
-        System.out.println("\u001B[36m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+        System.out.println("\u001B[38m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
         String[] mainLogo = new String[12];
         mainLogo[0] = "         ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄       ▄▄        ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ";
         mainLogo[1] = "        ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░▌     ▐░░▌      ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌";
@@ -37,50 +43,28 @@ public class App {
         //명령어 코멘트 로고 밑에 출력
         System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
         System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█"); System.out.print("  메인 화면 명령어 목록  "); System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("비회원이 사용할 수 있는 명령어 : [공지사항] [장르별 추천] [로그인] [앱종료]                                    "); System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("회원이시면 사용할 수 있는 명령어가 많아집니다.                                                                 "); System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("계정이 없으시다면 명령어 입력 란에 [회원가입]을 입력하여 계정을 생성해주세요.                                  "); System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("계정이 있으시면 명령어 입력 란에 [로그인]을 입력하여 로그인을 해주세요."); System.out.print("\u001B[33m                 ▌ \n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("4.     "); System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ "); System.out.print("5.     "); System.out.print("\u001B[33m▌ \n");
+        System.out.print("\u001B[33m ▌ "); System.out.print("비회원이 사용할 수 있는 명령어 : [장르별] [회원가입] [로그인] [앱종료]                                         "); System.out.print("\u001B[33m▌ \n");
+        System.out.print("\u001B[33m ▌ "); System.out.print("회원이면 사용할 수 있는 명령어가 많아집니다.                                                                   "); System.out.print("\u001B[33m▌ \n");
+        System.out.print("\u001B[33m ▌ "); System.out.print("계정이 없다면 명령어 입력 란에 [회원가입]을 입력하여 계정을 생성해주세요.                                      "); System.out.print("\u001B[33m▌ \n");
+        System.out.print("\u001B[33m ▌ "); System.out.print("계정이 있으면 명령어 입력 란에 [로그인]을 입력하여 로그인을 해주세요.                                          "); System.out.print("\u001B[33m▌ \n");
         System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
 
+
+
         //명령어 입력란(맨 마지막 줄에 출력)
-        System.out.println("\u001B[38m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
-        System.out.print("\u001B[38m ▌ 명령어 입력 : "); String cmd = sc.nextLine(); System.out.println("                                                                    ▌");
-        System.out.println("\u001B[37m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+        while (true) {
 
+            System.out.println("\u001B[38m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+            System.out.print("\u001B[38m ▌ 명령어 입력 : ");
+            String cmd = sc.nextLine();
+            cmd = cmd.trim();
 
+            if (cmd.length() == 0) {
+                continue;
+            }
+            if (cmd.equals("앱종료")) {
+                break;
+            }
+        }
     }
-//    public void doAction(String cmd) {
-//        this.cmd = cmd;
-//
-//        switch ( actionMethodName ) {
-//            case "write":
-//                doWrite();
-//                break;
-//            case "list":
-//                showList();
-//                break;
-//            case "detail":
-//                showDetail();
-//                break;
-//            case "modify":
-//                doModify();
-//                break;
-//            case "delete":
-//                doDelete();
-//                break;
-//            case "currentBoard":
-//                doCurrentBoard();
-//                break;
-//            case "changeBoard":
-//                doChangeBoard();
-//                break;
-//            default:
-//                System.out.println("존재하지 않는 명령어 입니다.");
-//                break;
-//        }
-//    }
-
 }
