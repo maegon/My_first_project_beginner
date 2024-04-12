45369112106d9b253488e1e0cbbc9cf1175c307c
maegon <dlagorhs99@naver.com>
2024-04-12 14:54:55 +0900

    음악검색시 ë음악이 비워져있으면 음악이 없습니다 출력 완료, 음악을 musicList에 넣어서 음악 검색시 ì출력이 올바ë르게 작동하도록 구현중

--- src/main/java/org/example/controller/MusicController.java
+++ src/main/java/org/example/controller/MusicController.java
@@ -54,60 +54,74 @@ public abstract class MusicController extends Controller {
 
 
     public static void doSearchMusic() {
-        if (isLogined() == false ) {
-            System.out.println("\u001B[31m ▌ 로그인 상태가 아닙니다.");
-            return;
-        }
-        if ( musics.size() == 0 ) {
-            System.out.println("\u001B[35m ▌ 음악이 없습니다.");
-            return;
-        }
-        if (isLogined() == true ) {
-            System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
-            System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█"); System.out.print("  음악 검색 명령어 가이드  "); System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
-            System.out.print("\u001B[33m ▌ "); System.out.print("음악제목으로 검색 : '음악제목' 형식으로 '소문자'로 작성 (예시: hope)                                                    "); System.out.print("\u001B[33m▌ \n");
-            System.out.print("\u001B[33m ▌ "); System.out.print("아티스트명으로 검색 : '아티스트명' 형식으로 '소문자'로 작성 (예시: todu)                                                       "); System.out.print("\u001B[33m▌ \n");
-            System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+        try {
+            if (isLogined() == false) {
+                System.out.println("\u001B[31m ▌ 로그인 상태가 아닙니다.");
+                return;
+            }
+            if (musics.size() == 0) {
+                System.out.println("\u001B[35m ▌ 음악이 없습니다.");
+                return;
+            }
+            if (isLogined() == true) {
+                System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+                System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+                System.out.print("  음악 검색 명령어 가이드  ");
+                System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
+                System.out.print("\u001B[33m ▌ ");
+                System.out.print("음악제목으로 검색 : '음악제목' 형식으로 '소문자'로 작성 (예시: hope)                                                    ");
+                System.out.print("\u001B[33m▌ \n");
+                System.out.print("\u001B[33m ▌ ");
+                System.out.print("아티스트명으로 검색 : '아티스트명' 형식으로 '소문자'로 작성 (예시: todu)                                                       ");
+                System.out.print("\u001B[33m▌ \n");
+                System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
 
-            System.out.print("\u001B[38m ▌ 검색어 : ");
-            String searchKeyword = sc.nextLine();
+                System.out.print("\u001B[38m ▌ 검색어 : ");
+                String searchKeyword = sc.nextLine();
 
-            List<Music> forListMusics = musics;
-            List<Artist> forListArtists = artists;
+                List<Music> forListMusics = musics;
+                List<Artist> forListArtists = artists;
 
-            if (searchKeyword.length() > 0) {
-                forListMusics = new ArrayList<>();
-                forListArtists = new ArrayList<>();
-                for (Music music : musics) {
-                    if (music.musicTitle.contains(searchKeyword)) {
-                        forListMusics.add(music);
+                if (searchKeyword.length() > 0) {
+                    forListMusics = new ArrayList<>();
+                    forListArtists = new ArrayList<>();
+                    for (Music music : musics) {
+                        if (music.musicTitle.contains(searchKeyword)) {
+                            forListMusics.add(music);
+                        }
+                    }
+                    for (Artist artist : artists) {
+                        if (artist.artistName.contains(searchKeyword)) {
+                            forListArtists.add(artist);
+                        }
+                    }
+                    if (forListMusics.size() == 0) {
+                        System.out.println("\u001B[35m ▌ 검색 결과가 존재하지 않습니다.");
+                        return;
                     }
                 }
-                for (Artist artist : artists) {
-                    if(artist.artistName.contains(searchKeyword)) {
-                        forListArtists.add(artist);
+                System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+                System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+                System.out.print("검색된 음악 목록");
+                System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
+                System.out.print("\u001B[33m ▌ ");
+                System.out.print(" 번호  ░  아티스트명  ░  조회수  ░  제목                                                                           ");
+                System.out.print("\u001B[33m ▌ \n");
+                for (int i = forListMusics.size() - 1; i >= 0; i--) {
+                    for (int j = forListArtists.size() - 1; j >= 0; j--) {
+                        Music music = forListMusics.get(i);
+                        Artist artist = forListArtists.get(j);
+                        System.out.printf("\u001B[33m ▌    %d   ░  %s  ░    %d     ░  %s  \n", music.id, artist.artistName, music.hit, music.musicTitle);
                     }
                 }
-                if(forListMusics.size() == 0) {
-                    System.out.println("\u001B[35m ▌ 검색 결과가 존재하지 않습니다.");
-                    return;
-                }
+                System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
             }
-            System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
-            System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█"); System.out.print("검색된 음악 목록"); System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
-            System.out.print("\u001B[33m ▌ "); System.out.print(" 번호  ░  아티스트명  ░  조회수  ░  제목                                                                           "); System.out.print("\u001B[33m ▌ \n");
-            for (int i = forListMusics.size() - 1; i >= 0; i--) {
-                for(int j = forListArtists.size() - 1; j >=0; j--) {
-                    Music music = forListMusics.get(i);
-                    Artist artist = forListArtists.get(j);
-                    System.out.printf("\u001B[33m ▌    %d   ░  %s  ░    %d     ░  %s  \n", music.id, artist.artistName, music.hit, music.musicTitle);
-                }
-            }
-            System.out.println("\u001B[33m"+" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
-
+        } catch (NullPointerException e) {
+            System.out.print("\u001B[31m ▌ 음악이 없습니다.\n");
         }
-
     }
+
+
 
 //    public static void main(String[] args) {
 //        MyPlayer mp = new MyPlayer("C:\\Jnathyn - Dioma [NCS Release].mp3");
