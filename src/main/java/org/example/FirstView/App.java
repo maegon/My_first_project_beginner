8bfb9fda840f34e6610342388a1c58e641f04bd7
maegon <dlagorhs99@naver.com>
2024-04-19 14:24:02 +0900

    DB_Name을 Mysql에 있는 데이터베이스명으로 변경

--- src/main/java/org/example/FirstView/App.java
+++ src/main/java/org/example/FirstView/App.java
@@ -10,7 +10,7 @@ import org.example.db.DBConnection;
 public class App {
 
     public App() {
-        DBConnection.DB_NAME = "sbs_proj";
+        DBConnection.DB_NAME = "ljw_beginner_pj_sbs";
         DBConnection.DB_USER = "sbsst";
         DBConnection.DB_PASSWORD = "sbs123414";
         DBConnection.DB_PORT = 3306;
