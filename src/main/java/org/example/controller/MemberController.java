283e4c43f9da72581cad4142bc302503012f91fd
maegon <dlagorhs99@naver.com>
2024-04-19 16:05:58 +0900

    로ê관리자가 아닌 회원으로 로그인시 null포인터오류 catch로 감싸 해결

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.example.container.Container;
 import org.example.dto.Member;
 import org.example.service.MemberService;
+
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 import java.util.Scanner;
 
 
@@ -51,101 +53,109 @@ public class MemberController extends Controller {
     }
 
     public void doJoin() {
-        int id = Container.memberDao.getNewId();
-        String loginId = null;
-        while (true) {
-            System.out.print("\u001B[38m ▌ 아이디 : ");
-            loginId = sc.nextLine();
+//        try {
+            int id = Container.memberDao.getNewId();
+            String loginId = null;
+            while (true) {
+                System.out.print("\u001B[38m ▌ 아이디 : ");
+                loginId = sc.nextLine();
 
-            if (isJoinableLoginId(loginId) == false) {
-                System.out.printf("\u001B[35m ▌ %s(은)는 이미 사용중인 아이디입니다.\n", loginId);
-                continue;
+                if (isJoinableLoginId(loginId) == false) {
+                    System.out.printf("\u001B[35m ▌ %s(은)는 이미 사용중인 아이디입니다.\n", loginId);
+                    continue;
+                }
+                break;
             }
-            break;
-        }
-        String loginPw = null;
-        String loginPwConfirm = null;
-        while (true) {
-            System.out.print("\u001B[38m ▌ 패스워드 : ");
-            loginPw = sc.nextLine();
-            System.out.print("\u001B[38m ▌ 패스워드 확인 : ");
-            loginPwConfirm = sc.nextLine();
-            if (loginPw.equals(loginPwConfirm) == false) {
-                System.out.println("\u001B[35m ▌ 비밀번호가 일치하지 않습니다.\n재입력해주세요.");
-                continue;
+            String loginPw = null;
+            String loginPwConfirm = null;
+            while (true) {
+                System.out.print("\u001B[38m ▌ 패스워드 : ");
+                loginPw = sc.nextLine();
+                System.out.print("\u001B[38m ▌ 패스워드 확인 : ");
+                loginPwConfirm = sc.nextLine();
+                if (loginPw.equals(loginPwConfirm) == false) {
+                    System.out.println("\u001B[35m ▌ 비밀번호가 일치하지 않습니다.\n재입력해주세요.");
+                    continue;
+                }
+                break;
             }
-            break;
-        }
-        String memberName = null;
-        while (true) {
-            System.out.print("\u001B[38m ▌ 활동 이름 : ");
-            memberName = sc.nextLine();
+            String memberName = null;
+            while (true) {
+                System.out.print("\u001B[38m ▌ 활동 이름 : ");
+                memberName = sc.nextLine();
 
-            if (isJoinableMemberName(memberName) == false) {
-                System.out.printf("\u001B[35m ▌ %s(은)는 이미 사용중인 활동 이름입니다.\n", memberName);
-                continue;
+                if (isJoinableMemberName(memberName) == false) {
+                    System.out.printf("\u001B[35m ▌ %s(은)는 이미 사용중인 활동 이름입니다.\n", memberName);
+                    continue;
+                }
+                break;
             }
-            break;
-        }
 
-        String adminId = null;
+            String adminId = null;
 
-        memberService.join(loginId, loginPw, memberName, adminId);
-        System.out.printf("\u001B[35m ▌ 회원가입이 완료되었습니다.\n\u001B[35m ▌ %s님 환영합니다.\n", memberName);
+            memberService.join(loginId, loginPw, memberName, adminId);
+            System.out.printf("\u001B[35m ▌ 회원가입이 완료되었습니다.\n\u001B[35m ▌ %s님 환영합니다.\n", memberName);
+//        }
+
     }
 
     public void doLogin() {
-        System.out.print("\u001B[38m ▌ 아이디 : ");
-        String loginId = sc.nextLine();
-        System.out.print("\u001B[38m ▌ 패스워드 : ");
-        String loginPw = sc.nextLine();
+        try {
 
-        Member member = memberService.getMemberByLoginId(loginId);
+            System.out.print("\u001B[38m ▌ 아이디 : ");
+            String loginId = sc.nextLine();
+            System.out.print("\u001B[38m ▌ 패스워드 : ");
+            String loginPw = sc.nextLine();
 
-        if (member == null) {
-            System.out.println("\u001B[31m ▌ 아이디가 일치하지 않습니다.");
-            return;
-        }
-        if (member.loginPw.equals(loginPw) == false) {
-            System.out.println("\u001B[31m ▌ 비밀번호가 일치하지 않습니다.");
-            return;
-        }
+            Member member = memberService.getMemberByLoginId(loginId);
 
-        if (member.id == 1) {
-            System.out.print("\u001B[38m ▌ 관리자번호 : ");
-            String adminId = sc.nextLine();
-            if (adminId.equals("SBS12341499JW") != true) {
-                System.out.println("\u001B[31m ▌ 관리자번호가 일치하지 않습니다.");
+            if (member == null) {
+                System.out.println("\u001B[31m ▌ 아이디가 일치하지 않습니다.");
                 return;
             }
-        }
+            if (member.loginPw.equals(loginPw) == false) {
+                System.out.println("\u001B[31m ▌ 비밀번호가 일치하지 않습니다.");
+                return;
+            }
 
-        session.setLoginedMember(member);
-        Member loginedMember = session.getLoginedMember();
+            if (member.id == 1) {
+                System.out.print("\u001B[38m ▌ 관리자번호 : ");
+                String adminId = sc.nextLine();
+                if (adminId.equals("SBS12341499JW") != true) {
+                    System.out.println("\u001B[31m ▌ 관리자번호가 일치하지 않습니다.");
+                    return;
+                }
+            }
 
-        System.out.printf("\u001B[35m ▌ 로그인되었습니다.\n\u001B[35m ▌ %s님 환영합니다.\n", loginedMember.memberName);
-        System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
-        System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
-        System.out.print("  회원 전용 명령어 목록  ");
-        System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
-        System.out.print("\u001B[33m ▌ ");
-        System.out.print("음악 관련 명령어 : [음악목록] [음악검색]                                                                       ");
-        System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ ");
-        System.out.print("게시물 관련 명령어 : [글목록] [글검색] [글보기] [글작성] [글수정] [글삭제]                                     ");
-        System.out.print("\u001B[33m▌ \n");
-        System.out.print("\u001B[33m ▌ ");
-        System.out.print("기능 관련 명령어 : [로그아웃] [앱종료]                                                              ");
-        System.out.print("\u001B[33m▌ \n");
-        if (member.adminId.equals("SBS12341499JW") == true) {
-            System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
-            System.out.print("  관리자 전용 명령어 목록  ");
+            session.setLoginedMember(member);
+            Member loginedMember = session.getLoginedMember();
+
+            System.out.printf("\u001B[35m ▌ 로그인되었습니다.\n\u001B[35m ▌ %s님 환영합니다.\n", loginedMember.memberName);
+            System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+            System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+            System.out.print("  회원 전용 명령어 목록  ");
             System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
             System.out.print("\u001B[33m ▌ ");
-            System.out.print("음악 관련 명령어 : [음악추가]                                                                                  ");
+            System.out.print("음악 관련 명령어 : [음악목록] [음악검색]                                                                       ");
             System.out.print("\u001B[33m▌ \n");
+            System.out.print("\u001B[33m ▌ ");
+            System.out.print("게시물 관련 명령어 : [글목록] [글검색] [글보기] [글작성] [글수정] [글삭제]                                     ");
+            System.out.print("\u001B[33m▌ \n");
+            System.out.print("\u001B[33m ▌ ");
+            System.out.print("기능 관련 명령어 : [로그아웃] [앱종료]                                                                         ");
+            System.out.print("\u001B[33m▌ \n");
+            if (member.adminId.equals("SBS12341499JW") == true) {
+                System.out.print("\u001B[33m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+                System.out.print("  관리자 전용 명령어 목록  ");
+                System.out.print("\u001B[33m █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n");
+                System.out.print("\u001B[33m ▌ ");
+                System.out.print("음악 관련 명령어 : [음악추가]                                                                                  ");
+                System.out.print("\u001B[33m▌ \n");
+            }
+            System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+        } catch (NullPointerException e) {
+
         }
-        System.out.println("\u001B[33m" + " ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
     }
 
     private void doLogOut() {
