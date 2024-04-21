41c8b1959afb96c4cb51554ac55f5f33d68dfe8d
meagon <dlagorhs99@naver.com>
2024-04-21 12:54:39 +0900

    db 백업 완료

--- src/main/java/org/example/db/ljw_pj_sql_db.sql
+++ src/main/java/org/example/db/ljw_pj_sql_db.sql
@@ -29,14 +29,13 @@ CREATE TABLE `article` (
   `memberName` char(100) NOT NULL,
   `hit` int(100) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `article` */
 
 insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberName`,`hit`) values 
-(2,'2024-04-20 20:40:26','2024-04-20 20:40:26','제목2','내용2','user1',23),
-(3,'2024-04-20 20:49:23','2024-04-20 20:49:23','하이','그래','관리자',0),
-(4,'2024-04-21 01:37:45','2024-04-21 01:37:45','하이','그려','user1',0);
+(1,'2024-04-21 11:34:03','2024-04-21 11:34:03','제목1','내용1','관리자',14),
+(3,'2024-04-21 11:55:57','2024-04-21 11:55:57','테스트글작성 유저1번','테스트','user1',0);
 
 /*Table structure for table `articleReply` */
 
@@ -51,26 +50,16 @@ CREATE TABLE `articleReply` (
   `articleId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `articleId` (`articleId`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `articleReply` */
 
 insert  into `articleReply`(`id`,`regDate`,`updateDate`,`body`,`memberName`,`articleId`) values 
-(1,'2024-04-20 20:40:30','2024-04-20 20:40:30','관리자댓글1','관리자',1),
-(2,'2024-04-20 20:40:30','2024-04-20 20:40:30','유저댓글1','user1',2),
-(3,'2024-04-20 20:40:50','2024-04-20 20:40:50','유저댓글2','user1',2),
-(4,'2024-04-20 20:41:18','2024-04-20 20:41:18','하이','관리자',2),
-(5,'2024-04-20 20:45:47','2024-04-20 20:45:47','하이','관리자',2),
-(6,'2024-04-20 22:46:01','2024-04-20 22:46:01','하이','관리자',3),
-(7,'2024-04-21 00:55:42','2024-04-21 00:55:42','하이','관리자',3),
-(8,'2024-04-21 01:20:24','2024-04-21 01:20:24','하이','관리자',2),
-(9,'2024-04-21 01:26:02','2024-04-21 01:26:02','하이','관리자',2),
-(10,'2024-04-21 01:31:48','2024-04-21 01:31:48','하이','관리자',2),
-(11,'2024-04-21 01:38:00','2024-04-21 01:38:00','허허허허허','user1',3),
-(12,'2024-04-21 01:45:33','2024-04-21 01:45:33','언제쯤 자려나','관리자',2),
-(13,'2024-04-21 01:47:48','2024-04-21 01:47:48','이제 됐나?','관리자',2),
-(14,'2024-04-21 01:51:53','2024-04-21 01:51:53','나 못잘거같넹','관리자',2),
-(15,'2024-04-21 02:03:24','2024-04-21 02:03:24','드디어 됐다','user1',2);
+(1,'2024-04-21 11:34:11','2024-04-21 11:34:11','관리자댓글1','관리자',1),
+(2,'2024-04-21 11:34:11','2024-04-21 11:34:11','유저댓글1','user1',2),
+(3,'2024-04-21 11:34:11','2024-04-21 11:34:11','유저댓글2','user1',2),
+(4,'2024-04-21 11:54:22','2024-04-21 11:54:22','테스트댓글','나유저2',2),
+(5,'2024-04-21 11:57:02','2024-04-21 11:57:02','짬뽕?','user1',2);
 
 /*Table structure for table `member` */
 
@@ -87,13 +76,15 @@ CREATE TABLE `member` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `loginId` (`loginId`),
   UNIQUE KEY `memberName` (`memberName`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `member` */
 
 insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`memberName`,`adminId`) values 
-(1,'2024-04-20 20:41:00','2024-04-20 20:41:00','admin','admin','관리자','SBS12341499JW'),
-(2,'2024-04-20 20:41:00','2024-04-20 20:41:00','user1','user1','user1','null');
+(1,'2024-04-21 11:34:20','2024-04-21 11:34:20','admin','admin','관리자','SBS12341499JW'),
+(2,'2024-04-21 11:34:20','2024-04-21 11:34:20','user1','user1','user1','null'),
+(3,'2024-04-21 11:44:31','2024-04-21 11:44:31','user3','user3','나유저3','null'),
+(4,'2024-04-21 11:46:42','2024-04-21 11:46:42','user2','user2','나유저2','null');
 
 /*Table structure for table `track` */
 
@@ -107,15 +98,17 @@ CREATE TABLE `track` (
   `musicTitle` char(100) NOT NULL,
   `hit` int(100) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `track` */
 
 insert  into `track`(`id`,`regDate`,`updateDate`,`importMusicFile`,`musicTitle`,`hit`) values 
-(1,'2024-04-20 20:40:56','2024-04-20 20:40:56','Diamond Eyes - Stay.mp3','Diamond Eyes - Stay',20),
-(2,'2024-04-20 20:40:56','2024-04-20 20:40:56','Jim Yosef & Shiah Maisel - Just Getting Started.mp3','Jim Yosef & Shiah Maisel - Just Getting Started',5),
-(3,'2024-04-20 20:47:23','2024-04-20 20:47:23','Jnathyn - Clockwork.mp3','Jnathyn - Clockwork',0),
-(4,'2024-04-21 01:13:38','2024-04-21 01:13:38','Zedd - Clarity.mp3','Zedd - Clarity',0);
+(1,'2024-04-21 11:36:13','2024-04-21 11:36:13','Diamond Eyes - Stay.mp3','Diamond Eyes - Stay',20),
+(2,'2024-04-21 11:36:13','2024-04-21 11:36:13','Jim Yosef & Shiah Maisel - Just Getting Started.mp3','Jim Yosef & Shiah Maisel - Just Getting Started',5),
+(3,'2024-04-21 11:36:13','2024-04-21 11:36:13','Jnathyn - Dioma.mp3','Jnathyn - Dioma',16),
+(4,'2024-04-21 11:37:29','2024-04-21 11:37:29','Jnathyn - Rise.mp3','Jnathyn - Rise',0),
+(5,'2024-04-21 11:52:49','2024-04-21 11:52:49','Jnathyn - Clockwork.mp3','Jnathyn - Clockwork',0),
+(6,'2024-04-21 11:59:54','2024-04-21 11:59:54','Zedd & Alessia Cara - Stay.mp3','Zedd & Alessia Cara - Stay',0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
